3dabc5d3a6f08f8d68c3a3f90ada4b016c608a5c
Alois Barreras <alois@vicino.io>
2016-09-30 00:06:17 -0400

    updated deps

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Analytics = require('@astronomerio/analytics.js-core').constructor;
-var integration = require('@segment/analytics.js-integration');
+var integration = require('@astronomerio/analytics.js-integration');
 var sandbox = require('@segment/clear-env');
 var tester = require('@segment/analytics.js-integration-tester');
 var Rubicon = require('../lib');
